4f00469a7598da3e71ebfecdcd72e0c724d5fe6c
Luana-Fernandez <lufernandezvargas@gmail.com>
2025-06-17 08:52:58 -0300

    inicio

---
+++ TPI/src/main/java/com/mycompany/tpi/Modelos/Carrera.java
@@ -0,0 +1,12 @@
+package com.mycompany.tpi.Modelos;
+
+
+class Carrera {
+    private int idCarrera;
+    private String categoria;
+    private String horaInicio;
+    private String horaFin;
+    private String ubicacion;
+    private String detalle;
+    private 
+}
---
+++ TPI/src/main/java/com/mycompany/tpi/TPI.java
@@ -0,0 +1,9 @@
+package com.mycompany.tpi;
+
+
+public class TPI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
